2688e8ba194de21f9f63ddd45fe73f3f5dc299e1
natyele <natyelebertolanii@gmail.com>
2019-08-01 15:30:24 -0300

    adicionar formulario

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let nome = document.querySelector("#nome");
 let email = document.querySelector("#email");
-let confimacaoEmail = document.querySelector("#confimacao de email");
+let confimacaoEmail = document.querySelector("#confimacaoEmail");
 let senha = document.querySelector("#senha");
 let botao = document.querySelector("#BOTAO");
 let sexo = document.querySelector("#sexo");
+let div = document.querySelector("div");
+let section = document.querySelector("section");
+let h2 = document.querySelector("h2");
+let a = document.querySelector("a");
 
 function cadastro() {
-    if(email.value === confimacaoEmail.value){
+    if(email.value !== "" && email.value === confimacaoEmail.value){
         localStorage.setItem("sexo" ,sexo.value);
         localStorage.setItem("nome" , nome.value);
         localStorage.setItem("email" ,email.value);
-        localStorage.setItem("confimacao de email" ,confimacaoEmail.value); 
+        localStorage.setItem("confimacaoEmail" ,confimacaoEmail.value); 
         localStorage.setItem("senha" ,senha.value);
+        div.style.display = "none";
+        section.style.display = "initial";
+        h2.inner.HTML = `Seja bem vindo ${localStorage.nome}`;
+        a.inner.HTML = `Seu email e ${localStorage.email}`;
+    }else{
+        alert("erro")
     }
+}
+function resetar(){
+    localStorage.clear();
+
 }
 
 botao.onclick = cadastro;
+a.onclick = resetar;
